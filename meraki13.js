d6ae3be57b58ce198913fd5cc0b00116e98dc38d
sai33-23 <84378922+sai33-23@users.noreply.github.com>
2021-11-14 21:45:34 +0530

    Add files via upload

---
+++ meraki13.js
@@ -0,0 +1,6 @@
+var i=156;
+while (i<166){
+    console.log(i-155);
+    i+=1
+    }
+    
---
+++ meraki9.js
@@ -0,0 +1,7 @@
+var sum = 0;
+for (var i = 1; i <= 10; i++){
+    const input=require("readline.sync")
+    let num=input.questionInt("enter the number")
+    sum=sum+num 
+    };
+console.log(num);
